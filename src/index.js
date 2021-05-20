f27030dc6c0b9a27ae2a961b436bdcef59debf69
Rene <renegarcia1864@gmail.com>
2021-05-19 21:31:21 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+function challege1() {
+    return fetch(imgUrl)
+    .then(resp =>json())
+    .then(json => console.log(json))
+}
